843c92a0ae6d6b7b11c46b7151f52fce03ee4040
Samar-Siddique <samar.sadeeq7550@gmail.com>
2022-09-11 11:57:07 +0500

    updated userController

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -26,31 +26,29 @@ const register =async (req, res) => {
 		await new User({ ...req.body, password: hashPassword }).save();
 		res.status(201).send({ message: "User created successfully" });
 	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
+		res.status(500).send({ message: "Internal Server Error1" });
 	}
 }
 
 const login=async (req, res) => {
 	try {
-		const { error } = validateauth(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
-
-		const user = await User.findOne({ email: req.body.email });
-		if (!user)
-			return res.status(401).send({ message: "Invalid Email or Password" });
-
-		const validPassword = await bcrypt.compare(
-			req.body.password,
-			user.password
-		);
-		if (!validPassword)
-			return res.status(401).send({ message: "Invalid Email or Password" });
-
-		const token = user.generateAuthToken();
-		res.status(200).send({ data: token, message: "logged in successfully" });
+			const{email, password} = req.body
+			const user = await User.findOne({ "email": email });
+			
+			if(user){
+				const validPassword = await bcrypt.compare(password, user.password);
+				if (!validPassword)
+					return res.status(401).send({ message: "Invalid Password" });
+				else{
+					return res.status(200).json(user);
+				}
+			}
+			else{
+				return res.status(400).json("Not valid email.")
+			}
+		
 	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
+		res.status(500).send({ message: "Internal Server Error2" });
 	}
 }
 
